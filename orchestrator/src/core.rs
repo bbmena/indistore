a7f59b996976caf826f160e4a3a43e13b2cbd7c9
Chris <bbmena@gmail.com>
2023-08-01 02:35:02 -0600

    clean up messages files

--- orchestrator/src/core.rs
+++ orchestrator/src/core.rs
@@ -1,3 +1,4 @@
 pub mod hash_ring;
+pub mod messages;
 pub mod router;
 pub mod server;
---
+++ orchestrator/src/core/messages.rs
@@ -0,0 +1,25 @@
+use bytes::BytesMut;
+use std::net::IpAddr;
+use tachyonix::Sender;
+use uuid::Uuid;
+
+pub enum RouterCommand {
+    Shutdown(),
+    AddNode(IpAddr),
+    Subscribe(ChannelSubscribe),
+    Unsubscribe(ChannelUnsubscribe),
+}
+
+pub struct RouterRequestWrapper {
+    pub channel_id: Uuid,
+    pub body: BytesMut,
+}
+
+pub struct ChannelSubscribe {
+    pub channel_id: Uuid,
+    pub response_channel: Sender<BytesMut>,
+}
+
+pub struct ChannelUnsubscribe {
+    pub channel_id: Uuid,
+}
